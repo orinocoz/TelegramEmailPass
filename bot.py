eba8ef1dfcfe96ef51836098b49e81b6a037c279
tenet <furens@protonmail.com>
2021-02-24 08:27:59 -0500

    &777

--- bot.py
+++ bot.py
@@ -14,35 +14,40 @@ def send_welcome(message):
     item_disclam = types.InlineKeyboardButton(text='README', callback_data='readme')
 
     markup_inline.add(item_contact, item_disclam)
-    img = open('Sova.jpg', 'rb')
+    img = open('logo.jpg', 'rb')
     bot.send_photo(message.chat.id, img)
-    bot.send_message(message.chat.id, 'Вся информация взята из открытых источников \n И только в ознакомительных целях',
-                     reply_markup=markup_inline)
+    bot.send_message(message.chat.id, 'Вся информация взята из открытых источников '
+                                      '\n И только в ознакомительных целях', reply_markup=markup_inline)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def answer(call):
     if call.data == 'about':
-        bot.send_message(call.message.chat.id, 'Напишите нам сюда: test@test.com')
+        bot.send_message(call.message.chat.id, 'По всем вопросам: @b0t_for_you')
     elif call.data == 'readme':
         bot.send_message(call.message.chat.id,
-                         'Я умею искать слитые в сеть пароли и почту. \n Введи почту, пароль или телефон.')
+                         'Я умею искать слитые в сеть пароли и почту. \n Введи почту для поиска.')
 
 
 @bot.message_handler(content_types=['text'])
 def main_func(message):
     """Функция принимает текст от пользователя и ищет в базе"""
-
+    with open('log.txt', 'a', encoding='utf-8', errors='ignore') as file_log:
+        file_log.writelines(message.text + '\n')
+        file_log.close()
     # TODO: сделать регулярное выражение на входе чтобы принимала программа только почту
     # TODO: сделать чтобы закрывали звездочки пароль
     # TODO: организовать логирование
-    # TODO: фото можно подцепить из URL в соц сетях модифицировав её на основе почты
+    # TODO: фото можно подцепить из URL в соц сетях модифицировав её на основе почты цели
 
     os.chdir(base_catalog)
     try:
         cod_search = subprocess.run(['./query.sh', message.text], stdout=subprocess.PIPE)
-        file_simple = cod_search.stdout.decode('utf-8')
+        file_simple = cod_search.stdout.decode('utf-8', errors='ignore')
         bot.send_message(message.chat.id, file_simple)
+    except telebot.apihelper.ApiException:
+        bot.send_message(message.chat.id, 'Нет совпадений')
+        print('ApiException')
     except Exception as err:
         print(err)
 
